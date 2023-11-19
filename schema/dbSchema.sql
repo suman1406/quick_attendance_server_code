64de27cb85a0150884ad4c4a164b5701cd41c00c
Suman Panigrahi <psuman1406@gmail.com>
2023-11-19 10:23:12 +0530

    Schema reordered

--- schema/dbSchema.sql
+++ schema/dbSchema.sql
@@ -1,15 +1,16 @@
--- Table 1: Professors Table
--- userRole: 0 -> professor, 1 -> admin
+-- Table 1: Course Table
 -- isActive: 1 -> Active, 0 -> Deactivated.
--- attdStatus: 0 -> Absent, 1 -> Present, 2 -> OD, 3 -> ML, 4 -> Other
--- Table 1: Professors Table
--- userRole: 0 -> professor, 1 -> admin.
 CREATE TABLE IF NOT EXISTS course (
     courseID INT AUTO_INCREMENT PRIMARY KEY,
     courseName VARCHAR(255) NOT NULL,
     isActive CHAR(1) NOT NULL DEFAULT '1',
     UNIQUE (courseName)
 );
+-- Table 2: Users Table
+-- userRole: 0 -> professor, 1 -> admin
+-- isActive: 1 -> Active, 0 -> Deactivated.
+-- attdStatus: 0 -> Absent, 1 -> Present, 2 -> OD, 3 -> ML, 4 -> Other
+-- userRole: 0 -> professor, 1 -> admin.
 CREATE TABLE IF NOT EXISTS USERDATA (
     profID INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
@@ -18,31 +19,7 @@ CREATE TABLE IF NOT EXISTS USERDATA (
     userRole CHAR(1) NOT NULL,
     isActive CHAR(1) NOT NULL DEFAULT '1',
     courseID INT,
-    classID INT,
-    CONSTRAINT chk_user_course
-        CHECK ((userRole = '0' AND courseID IS NOT NULL AND classID IS NOT NULL) OR (userRole = '1' AND classID IS NULL)),
-    FOREIGN KEY (courseID) REFERENCES course(courseID),
-    FOREIGN KEY (classID) REFERENCES class(classID)
-);
--- This will act like a temporary table. Once the student verifies their email, the data will be moved to studentData table.
-CREATE TABLE USERREGISTER (
-    id INT NOT NULL AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL,
-    otp VARCHAR(6) NOT NULL,
-    createdAt TIMESTAMP NOT NULL,
-    PRIMARY KEY (id)
-);
--- Table 2: Students Table
--- isActive: 1 -> Active, 0 -> Deactivated.
--- Section: A, B, C, D, E, F
--- batchYear: 20XX -> Passing out year
-CREATE TABLE studentData (
-    -- StdID INT AUTO_INCREMENT PRIMARY KEY,
-    RollNo VARCHAR(20) NOT NULL UNIQUE,
-    StdName VARCHAR(255) NOT NULL,
-    classID INT,
-    isActive CHAR(1) NOT NULL DEFAULT '1',
-    FOREIGN KEY (classID) REFERENCES class(classID)
+    FOREIGN KEY (courseID) REFERENCES course(courseID)
 );
 -- Table 3: Class Table
 CREATE TABLE IF NOT EXISTS class (
@@ -50,24 +27,40 @@ CREATE TABLE IF NOT EXISTS class (
     batchYear INT NOT NULL,
     Dept VARCHAR(255) NOT NULL,
     Section VARCHAR(10) NOT NULL,
-    profID INT,
     courseID INT,
     Semester INT NOT NULL,
     isActive CHAR(1) NOT NULL DEFAULT '1',
-    unique index(classID),
-    FOREIGN KEY (profID) REFERENCES USERDATA(profID),
+    UNIQUE INDEX (classID),
     FOREIGN KEY (courseID) REFERENCES course(courseID)
 );
--- Table 4
-CREATE TABLE Slots (
+-- Table 4: ProfessorClass Table
+CREATE TABLE IF NOT EXISTS ProfessorClass (
+    professorID INT,
+    classID INT,
+    FOREIGN KEY (professorID) REFERENCES USERDATA(profID),
+    FOREIGN KEY (classID) REFERENCES class(classID)
+);
+-- Table 5: Students Table
+-- isActive: 1 -> Active, 0 -> Deactivated.
+-- Section: A, B, C, D, E, F
+-- batchYear: 20XX -> Passing out year
+CREATE TABLE IF NOT EXISTS studentData (
+    RollNo VARCHAR(20) NOT NULL UNIQUE,
+    StdName VARCHAR(255) NOT NULL,
+    classID INT,
+    isActive CHAR(1) NOT NULL DEFAULT '1',
+    FOREIGN KEY (classID) REFERENCES class(classID)
+);
+-- Table 6: Slots Table
+CREATE TABLE IF NOT EXISTS Slots (
     slotID INT AUTO_INCREMENT PRIMARY KEY,
     classID INT,
     periodNo INT NOT NULL,
     isActive CHAR(1) NOT NULL DEFAULT '1',
     FOREIGN KEY (classID) REFERENCES class(classID)
 );
--- Table 5
-CREATE TABLE if not exists attendance (
+-- Table 7: Attendance Table
+CREATE TABLE IF NOT EXISTS attendance (
     RollNo VARCHAR(20),
     attdStatus CHAR(1) NOT NULL,
     timestamp TIMESTAMP NOT NULL,
@@ -75,6 +68,15 @@ CREATE TABLE if not exists attendance (
     PRIMARY KEY (RollNo, timestamp),
     FOREIGN KEY (RollNo) REFERENCES studentData(RollNo),
     FOREIGN KEY (slotID) REFERENCES Slots(slotID)
+);
+-- Table 8:: UserRegister Table
+-- This will act like a temporary table. Once the student verifies their email, the data will be moved to studentData table.
+CREATE TABLE USERREGISTER (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL,
+    otp VARCHAR(6) NOT NULL,
+    createdAt TIMESTAMP NOT NULL,
+    PRIMARY KEY (id)
 );
 /*
  1. Login
