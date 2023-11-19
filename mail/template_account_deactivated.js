633f67b4338d0096ad1af49fca7050f91017d935
Suman Panigrahi <psuman1406@gmail.com>
2023-11-19 10:08:36 +0530

    mail template updated

--- mail/template_account_deactivated.js
+++ mail/template_account_deactivated.js
@@ -5,7 +5,7 @@ const TEMPLATE_ACCOUNT_DEACTIVATED = (managerEmail, managerName) => {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Amrita Placement Tracker OTP</title>
+        <title>Amrita Quick Attendance OTP</title>
         <style>
             * {
                 margin: 0;
